7edfd5e8b6db55b9f53405d071d7c841dd9f5279
Smita <smita.mokashe@gmail.com>
2024-02-21 20:46:47 +0530

    Access specifier

---
+++ AccessSpecifierC1.java
@@ -0,0 +1,12 @@
+package accessSpecifier;
+
+public class AccessSpecifierC1 {
+
+	public static void main(String[] args) {
+		System.out.println("Assignment 58: WAP to access 4 different access specifiers within the package"); 
+		AccessSpecifierC2 ac2 = new AccessSpecifierC2();
+		ac2.add();
+		ac2.sub();
+		ac2.div();
+	}
+}
